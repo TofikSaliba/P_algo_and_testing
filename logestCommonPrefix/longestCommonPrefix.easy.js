7cb923587cfe259d73bb183964671f20d1f0d559
TofikSaliba <tofik.saliba@gmail.com>
2022-04-21 16:48:33 +0300

    opened repo

---
+++ logestCommonPrefix/longestCommonPrefix.easy.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ *
+ * @description {Write a function to find the longest common prefix string amongst an array of strings.
+                If there is no common prefix, return an empty string "".}
+ * 
+ * Example 1 
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ * 
+ *
+ * Example 2
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ *
+ */
+const strs1 = ["flower", "flow", "flight"],
+  strs2 = ["dog", "racecar", "car"];
+
+const longestCommonPrefix = function (strs1) {
+  let firstTwo = strs1[0].slice(0, 2);
+  for (let i = 1; i < strs1.length; i++) {
+    if (firstTwo !== strs1[i].slice(0, 2)) {
+      return "";
+    }
+  }
+  return firstTwo;
+};
+
+console.log(longestCommonPrefix(strs1));
+
+module.exports = longestCommonPrefix;
